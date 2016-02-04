fb219a36064b87d1b0f488f0e719e11ea96a2c78
thumbimigwe <thumbimigwe@gmail.com>
2016-02-04 06:31:00 +0300

    selector

---
+++ PycharmProjects/selecting-code.py
@@ -0,0 +1,11 @@
+# selecting code and values with conditionals
+def main():
+	a, b = 2, 7
+	if a < b:
+		print ("a is less than b")
+	elif a > b:
+		print("a is greater than b")
+	else:
+		print("a is not less than b")
+
+if __name__ == '__main__':main()
