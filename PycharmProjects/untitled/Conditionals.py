ae9a597e9454c9d788d2bb23d29a8a99ceab83b2
Thoshh <thumbimigwe@gmail.comgit config --global user.name thumbimigwe>
2016-02-04 19:55:02 +0300

    conditionals

--- PycharmProjects/untitled/Conditionals.py
+++ PycharmProjects/untitled/Conditionals.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+a,b = 0,1
+if a < b:
+    print('a ({}) is less than b ({})'.format(a,b))
+else:
+    print('a({}) is not less than b({})'.format(a,b))
+
+print("Foo" if a < b else "bar")
+ 
