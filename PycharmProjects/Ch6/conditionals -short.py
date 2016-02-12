060c71bacd083f2695fe15f2756a5f3c79d043b5
thumbimigwe <thumbimigwe@gmail.com>
2016-02-12 04:45:31 +0300

    conditionals 2 short code

--- PycharmProjects/Ch6/conditionals -short.py
+++ PycharmProjects/Ch6/conditionals -short.py
@@ -1,12 +1,15 @@
  #!/usr/bin/python3
 def main():
     a , b = 1 , 1
-
-
-    if a < b:
-        v = 'this is true'
-    else:
-        v = 'this is not true'
+    v = 'this is true' if a < b else 'this is not true'
+    #
+    #
+    # if a < b:
+    #     v = 'this is true'
+    # else:
+    #     v = 'this is not true'
+    #
+    print(v)
 
 
 if __name__ == "__main__": main()
