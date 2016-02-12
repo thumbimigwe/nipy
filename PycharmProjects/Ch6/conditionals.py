ceb0b11a7785b55096a29bbc8b2b147d632886cf
thumbimigwe <thumbimigwe@gmail.com>
2016-02-12 04:27:30 +0300

    conditionALS

--- PycharmProjects/Ch6/conditionals.py
+++ PycharmProjects/Ch6/conditionals.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def main():
-    print("This is the conditionals.py file.")
+    v = "four"
+
+    if v == "one":
+        print("v is one")
+    elif v == "two":
+        print("v is two")
+    elif v == "three":
+        print("v is three")
+    else:
+        print("umeandika mavitu zako")
 
 if __name__ == "__main__": main()
